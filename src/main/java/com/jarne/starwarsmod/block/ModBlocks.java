8132f2405c43834c53c2cca0f202f91f476b6bbe
JarneAe <jarnea408@gmail.com>
2022-06-01 08:29:12 +0200

    Binley block (meme block just for testing)

--- src/main/java/com/jarne/starwarsmod/block/ModBlocks.java
+++ src/main/java/com/jarne/starwarsmod/block/ModBlocks.java
@@ -23,6 +23,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> CITRINE_BLOCK = registerBlock("citrine_block",
             ()-> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
 
+    public static final RegistryObject<Block> BINLEY_BLOCK = registerBlock("binley_block",
+            ()-> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(9f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block,CreativeModeTab tab){
         RegistryObject toReturn = BLOCKS.register(name,block);
